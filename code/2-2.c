8871444d21422db249fecbdfe6a928b1a6ff41e3
최준화 <choijunhwa072@gmail.com>
2026-04-16 11:03:04 +0900

    Update 2-2.c

--- code/2-2.c
+++ code/2-2.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
 int sum_2d_arr(int (*row)[3], int size) {
-  int sum = 0;
-  for(int i = 0; i < size; i++) {
-    for(int j = 0; j < sizeof(*row)/sizeof(int); j++) {
-      sum += row[j];
+    int sum = 0;
+    for(int i = 0; i < size; i++) {
+        for(int j = 0; j < sizeof(*row)/sizeof(int); j++) {
+            sum += *(row[i]) + j;
+        }
+        
     }
-    
-  }
-  return sum;
+    return sum;
 }
 
 int main() {
-  int arr[2][3] = {1,2,3,4,5,6};
+    int arr[2][3] = {1,2,3,4,5,6};
 
-  printf("%d\n", sum_2d_arr(arr, 2));
+    printf("%d\n", sum_2d_arr(arr, 2));
 }
